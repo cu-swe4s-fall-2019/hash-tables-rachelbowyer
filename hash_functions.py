aa34b5ec660fe99cf3f0db94799cb8d5f2d8ee5f
Rachel Bowyer <rabo8409@colorado.edu>
2019-10-17 02:48:04 +0000

    added hash functions and txt and plotting files

--- hash_functions.py
+++ hash_functions.py
@@ -1,6 +1,28 @@
+import sys
+
 
 def h_ascii(key, N):
-    return None
+    """ASCII value sum for characters in key"""
+    s = 0
+    for i in range(len(key)):
+        s += ord(key[i])
+    return s % N
 
 def h_rolling(key, N):
-    return None
+    '''polynomial rolling hash algorith,'''
+    s = 0
+    for i in range(len(key)):
+        s += ord(key[i]) * p**i
+    s = s % m
+    return s % N 
+
+
+if __name__ == '__main__':
+    
+    for l in open(sys.argv[1]):
+        if sys.argv[2] == 'ascii':
+            print(h_ascii_sum(l, 1000))
+        elif sys.argv[2] == 'rolling':
+            print(h_polynomial_rolling(l, 1000))
+        elif sys.argv[2] == 'python':
+            print(h_python(l, 1000))
